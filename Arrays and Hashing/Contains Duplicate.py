180939a52ea797fd9c7da7042e71f2c6d22011ef
unknown <mhoneylance@gmail.com>
2023-05-29 18:51:39 +0800

    Solve Contains Duplicate.py

---
+++ Arrays and Hashing/Contains Duplicate.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(n²)
+    # Space complexity: O(1)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        length = len(nums)
+        for i in range(length):
+            for j in range(i+1, length):
+                if (nums[i] == nums[j]):
+                    return True
+
+        return False
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_list = set()
+        for num in nums:
+            if num in unique_list:
+                return True
+            else:
+                unique_list.add(num)
+        return False
