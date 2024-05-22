fee60d53f8c22fae41777ea3e755646b723aa68b
Lance <mhoneylance@gmail.com>
2024-05-22 15:22:30 +0800

    Solve two-sum-ii-input-array-is-sorted.py

---
+++ Two Pointers/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)    
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
